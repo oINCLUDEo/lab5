8d8f7badf861b8a28f29e8df7ae7238b86533823
oINCLUDEo <77641549+oINCLUDEo@users.noreply.github.com>
2023-05-19 12:47:25 +0400

    Add files via upload

---
+++ lab5.py
@@ -0,0 +1,34 @@
+import timeit
+import matplotlib.pyplot as plt
+
+a = []
+b = []
+
+
+def paf(n):
+    if n == 1:
+        return 1
+    elif n > 1:
+        return paf(n - 1) * (2 * n + 1)
+
+
+def pav(n):
+    result = 1
+    for i in range(2, n + 1):
+        result *= (2 * i + 1)
+    return result
+
+
+nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 994]
+print('№' + '       ' + 'Рекурсивно' + '        ' + 'Итеративно')
+for i in nums:
+    a.append(timeit.timeit(lambda: paf(i), number=20000))
+    print(paf(i))
+    b.append(timeit.timeit(lambda: pav(i), number=20000))
+    print(i, ' ', a[-1], '|', b[-1])
+plt.xlabel('Числовые значения')
+plt.ylabel('Время поиска')
+plt.plot(nums, a, label='Рекурсивно')
+plt.plot(nums, b, label='Итеративно')
+plt.legend()
+plt.show()
